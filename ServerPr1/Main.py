6b734bcdf1bed8c38fa2abe399daa1ce517e9e47
maddun <maddun@gmail.com>
2011-07-27 16:16:04 +0400

    0.2 ver with graphics
    Signed-off-by: maddun <maddun@gmail.com>

--- ServerPr1/Main.py
+++ ServerPr1/Main.py
@@ -9,13 +9,7 @@ from PyQt4 import QtGui
 
 
 if __name__ == '__main__':
-    Rec1 = MyRectangle("Squa1reClient",[2,3],[3,4])
-    #Rec1.Move(1,3)
-    app = QtGui.QApplication(sys.argv)
-    wiget = QtGui.QWidget()
-    wiget.setWindowTitle("Server")
-    wiget.show()
+    Rec1 = MyRectangle("SquareClient",[2,3],[3,4])
     x = int(raw_input("Ввеедите смещение по X: \n"))
     y = int(raw_input("Ввеедите смещение по Y: \n"))
     Rec1.Move(x,y)
-    sys.exit(app.exec_())
--- ServerPr1/Server.py
+++ ServerPr1/Server.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 __author__ = 'comp124'
 import SocketServer
+import threading
 import sys
 from Rectangle import MyRectangle
-from PyQt4 import QtGui, QtCore
+from PyQt4 import QtGui, QtCore, uic
 
 class ServerRectHandler (SocketServer.BaseRequestHandler):
     """Обрабатывает сообщения отправлеяемые клиентом.
@@ -15,27 +16,76 @@ class ServerRectHandler (SocketServer.BaseRequestHandler):
         """
         self.data = self.request.recv(1024).strip()
         incoming = self.data.split(";")
-        MyRectangle.MoveOnServ(R1,int(incoming[0]),int(incoming[1]))
-        #print "Координаты %s" %self.data
-        print "Теперь координаты у сервера", R1.t1, "и", R1.t2
-        self.request.send(";".join([str(R1.t1[0]),str(R1.t1[1]),str(R1.t2[0]),str(R1.t2[1])]))
+        #Двигаем на сервере по полученным координатам
+        MyRectangle.MoveOnServ(self.server.R1,int(incoming[0]),int(incoming[1]))
+        print "Координаты %s" %self.data
+        print "Теперь координаты у сервера", self.server.R1.t1, "и", self.server.R1.t2
+        #Отсылаем координаты на клиент
+        self.request.send(";".join([str(self.server.R1.t1[0]),str(self.server.R1.t1[1]),str(self.server.R1.t2[0]),str(self.server.R1.t2[1])]))
 
-        
+class RectangleServer (SocketServer.TCPServer):
+    """ Класс реализует сервер управляющий собственным прямоугольником и изменяющий координаты по сообщению клиента
+    """
+    def __init__(self, name):
+        """ Инициализирует сервер. А также создает к нему прямоугольник
+        """
+        self.R1 = MyRectangle(name)
+        SocketServer.TCPServer.__init__(self, ('localhost',11000),ServerRectHandler)
+
+    def Starter(self):
+       """ Запускает сервер.
+       """
+       #ServThread = threading.Thread(target=MyServer.serve_forever)
+       #ServThread.setDaemon(True)
+       #print "Is server thread alive",ServThread.isAlive()
+       ServThread.start()
+       #print "Is server thread alive",ServThread.isAlive()
+
+    def Stopper(self):
+       """ Останавливает сервер.
+       """
+       #ServThread = threading.Thread(target=MyServer.serve_forever)
+       #ServThread.setDaemon(True)
+       #print "Is server thread alive",ServThread.isAlive()
+       self.shutdown()
+       #print "Is server thread alive",ServThread.isAlive()
+
+
+class ServerWindow (QtGui.QWidget):
+    """
+       Класс для окошка сервера.
+    """
+    def __init__(self,Server,parent = None):
+        """ Создаем собственное окошко
+        """
+        QtGui.QWidget.__init__(self,parent)
+        self.resize(150,90)
+        self.setWindowTitle('Server')
+        StartButton = QtGui.QPushButton('Start',self)
+        StartButton.setGeometry(10, 10, 60, 35)
+        #StopButton = QtGui.QPushButton('Stop',self)
+        #StopButton.setGeometry(80, 10, 60, 35)
+        self.connect(StartButton,QtCore.SIGNAL('clicked()'),Server.Starter)
+        #self.connect(StopButton,QtCore.SIGNAL('clicked()'),Server.Stopper)
+
+    def closeEvent(self, QCloseEvent):
+        MyServer.shutdown()
+        print "Server shutting down!!"
+        print "Server thread is a live", ServThread.isAlive()
+        QCloseEvent.accept()
+
 
 if   __name__ == '__main__':
     HOST, PORT = 'localhost', 11000
-    R1 = MyRectangle("ServerSquare")
-    MyServer = SocketServer.TCPServer((HOST,PORT),ServerRectHandler)
-   # print "Запускаю сервер!"
-   # MyServer.serve_forever()
-    Servapp = QtGui.QApplication(sys.argv)
-    wiget = QtGui.QWidget()
-    wiget.setGeometry(300,300,250,150)
-    wiget.setWindowTitle("Server")
-    StartButt = QtGui.QPushButton("Start",wiget)
-    StartButt.setGeometry(10, 10, 60, 35)
-    #wiget.connect(StartButt,QtCore.SIGNAL('clicked()'), QtGui.qApp, QtCore.SLOT('start()'))
-    StopButton = QtGui.QPushButton("Stop",wiget)
-    StopButton.setGeometry(10, 70, 60, 35)
-    wiget.show()
-    sys.exit(Servapp.exec_())
+    MyServer = RectangleServer("ServSquare")
+    #print "Запускаю сервер!"
+    ServThread = threading.Thread(target=MyServer.serve_forever)
+    ServAppl = QtGui.QApplication(sys.argv)
+    ServWindow = ServerWindow(MyServer)
+    #ServWindow = QtGui.QWidget()
+    #uic.loadUi("MainWind.ui",ServWindow)
+    ServWindow.show()
+    #MyServer.shutdown()
+    sys.exit(ServAppl.exec_())
+
+    
